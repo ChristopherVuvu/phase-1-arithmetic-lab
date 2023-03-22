6704d69cc9377924a04cf4748b7111864710e3a3
Christopher Vuvu <vuvumumo93@gmail.com>
2023-03-23 01:45:58 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+
+
+let random = Math.floor((Math.random()) *10)+1;
+
+let num3 = 15;
+let num4 = 11;
+let mod= num3%num4
+
+let max = Math.max (2,4,5,8,11,17,20);
